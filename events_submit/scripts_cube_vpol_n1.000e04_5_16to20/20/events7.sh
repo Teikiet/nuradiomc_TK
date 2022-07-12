6076116b99b27cb639d50dd1620767efa4629bb1
Kiet Tran <tkiet@submit-1.icecube.wisc.edu>
2022-07-12 15:55:55 -0500

    all

--- events_submit/scripts_cube_vpol_n1.000e04_5_16to20/20/events7.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/setup.sh`
-export PYTHONPATH=/cvmfs/ara.opensciencegrid.org/trunk/centos7/root_build/lib:/home/tkiet/NuRadioMC:
-python /home/tkiet/nuradiomc/events_submit/my_generate_events.py 20 20 1 7
