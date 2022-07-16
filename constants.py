aa0720621edc38ee56055e3bf2f2790b8eb765bd
Teikiet <ultimateantimatter@gmail.com>
2022-07-16 01:41:06 -0500

    make a deeper ara1

--- constants.py
+++ constants.py
@@ -34,9 +34,9 @@ NUM_EVENTS = 1e6
 NUM_EVENTS_PER_FILE = 1e3
 
 # Detector's json file
-DET_NAME = "surface"
+DET_NAME = "ara_focusing"
 DET_FILE = path.join(DET_PATH, f"{DET_NAME}_detector.json")
-DETECTOR = "/home/tkiet/nuradiomc/surface_station_1GHz.json"
+DETECTOR = "/home/tkiet/nuradiomc/ara1_deep.json"
 
 #Configuration:
 CONFIG = "/home/tkiet/nuradiomc/config_file_focusing.yaml"
